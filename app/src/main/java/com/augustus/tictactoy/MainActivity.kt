7f3b0b9eeb591dd1a7fe697d03afd81a5586bc70
west <augustusnwangwu@gmail.com>
2023-05-29 19:55:53 +0100

    Find Winner

--- app/src/main/java/com/augustus/tictactoy/MainActivity.kt
+++ app/src/main/java/com/augustus/tictactoy/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -29,7 +30,93 @@ class MainActivity : AppCompatActivity() {
             R.id.bu9 -> cellId = 9
         }
 
-        Log.d("buClick", buSelected.id.toString())
-        Log.d("buClick: cellId", cellId.toString())
+        //Log.d("buClick", buSelected.id.toString())
+        //Log.d("buClick: cellId", cellId.toString())
+
+        playGame(cellId, buSelected)
+    }
+
+    var activePlayer = 1
+
+    var player1 = ArrayList<Int>()
+    var player2 = ArrayList<Int>()
+
+    fun playGame(cellId:Int, buSelected:Button){
+        if( activePlayer == 1){
+            buSelected.text = "X"
+            buSelected.setBackgroundResource(R.color.blue)
+            player1.add(cellId)
+            activePlayer = 2
+        }else{
+            buSelected.text = "O"
+            buSelected.setBackgroundResource(R.color.darkGreen)
+            player2.add(cellId)
+            activePlayer = 1
+        }
+
+        buSelected.isEnabled = false
+
+        checkWinner()
+    }
+
+    fun checkWinner() {
+
+        var winner = -1
+
+        // row 1
+        if(player1.contains(1) && player1.contains(2) && player1.contains(3)){
+            winner = 1
+        }
+        if(player2.contains(1) && player2.contains(2) && player2.contains(3)){
+            winner = 2
+        }
+
+        // row 2
+        if(player1.contains(4) && player1.contains(5) && player1.contains(6)){
+            winner = 1
+        }
+        if(player2.contains(4) && player2.contains(5) && player2.contains(6)){
+            winner = 2
+        }
+
+        // row 3
+        if(player1.contains(7) && player1.contains(8) && player1.contains(9)){
+            winner = 1
+        }
+        if(player2.contains(7) && player2.contains(8) && player2.contains(9)){
+            winner = 2
+        }
+
+
+        // column 1
+        if(player1.contains(1) && player1.contains(4) && player1.contains(7)){
+            winner = 1
+        }
+        if(player2.contains(1) && player2.contains(4) && player2.contains(7)){
+            winner = 2
+        }
+
+
+        // column 2
+        if(player1.contains(2) && player1.contains(5) && player1.contains(8)){
+            winner = 1
+        }
+        if(player2.contains(2) && player2.contains(5) && player2.contains(5)){
+            winner = 2
+        }
+
+        // column 3
+        if(player1.contains(3) && player1.contains(6) && player1.contains(9)){
+            winner = 1
+        }
+        if(player2.contains(3) && player2.contains(6) && player2.contains(9)){
+            winner = 2
+        }
+
+        if(winner == 1){
+            Toast.makeText(this, "Player 1 win the game", Toast.LENGTH_LONG).show()
+        }else if(winner == 2){
+            Toast.makeText(this, "Player 2 win the game", Toast.LENGTH_LONG).show()
+        }
     }
 }
